00003a8a5e5fe0024e28696a6fbed5e72975e9d6
goya777 <goia.nata777@gmail.com>
2022-10-31 09:26:47 +0400

    davaleba

---
+++ src/test/java/davaleba4.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
+import jdk.jshell.Snippet;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class davaleba4 {
+    @Test
+    public void davaleba4 (){
+        open ("https://www.google.com/");
+        $(byClassName("gLFyf")).setValue("axalcixe").pressEnter();
+
+    }
+}
+
